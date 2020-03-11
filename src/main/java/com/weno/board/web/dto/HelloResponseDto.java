248af721b01db75b69a4cc0f4cc6d4655d35b148
“wenodev” <logicalstark1105@gmail.com>
2020-03-11 23:58:31 +0900

    chapter1, chapter2 test

---
+++ src/main/java/com/weno/board/web/dto/HelloResponseDto.java
@@ -0,0 +1,15 @@
+package com.weno.board.web.dto;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+
+}
