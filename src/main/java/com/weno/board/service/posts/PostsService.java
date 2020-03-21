b96b003beb6be16f4432d36532dc3175ee21bfd0
“wenodev” <logicalstark1105@gmail.com>
2020-03-22 02:34:06 +0900

    update&findByid

--- src/main/java/com/weno/board/service/posts/PostsService.java
+++ src/main/java/com/weno/board/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.weno.board.service.posts;
 
+import com.weno.board.domain.posts.Posts;
 import com.weno.board.domain.posts.PostsRepository;
+import com.weno.board.web.dto.PostsResponseDto;
 import com.weno.board.web.dto.PostsSaveRequestDto;
+import com.weno.board.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts= postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id ="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/weno/board/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,20 @@
+package com.weno.board.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+}
