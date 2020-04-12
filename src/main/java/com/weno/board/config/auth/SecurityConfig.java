0661b342d7faf64bc34cb7936eeeea5e3821d94d
“wenodev” <logicalstark1105@gmail.com>
2020-04-13 00:46:12 +0900

    CustomOAuth2UserService

--- src/main/java/com/weno/board/config/auth/SecurityConfig.java
+++ src/main/java/com/weno/board/config/auth/SecurityConfig.java
@@ -2,24 +2,29 @@ package com.weno.board.config.auth;
 
 import com.weno.board.domain.user.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityEnablerConfiguration {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").hasRole(Role.USER.name())
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
                 .logout()
@@ -27,7 +32,6 @@ public class SecurityConfig extends WebSecurityEnablerConfiguration {
                 .and()
                 .oauth2Login()
                 .userInfoEndpoint()
-                .userService(customOAuth2UserService);
+                .userService((OAuth2UserService<OAuth2UserRequest, OAuth2User>) customOAuth2UserService);
     }
-
 }
---
+++ src/main/java/com/weno/board/config/auth/dto/SessionUser.java
@@ -0,0 +1,8 @@
+package com.weno.board.config.auth.dto;
+
+import com.weno.board.domain.user.User;
+
+public class SessionUser {
+    public SessionUser(User user) {
+    }
+}
