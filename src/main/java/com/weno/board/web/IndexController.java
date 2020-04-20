ab18ce84cadeafcb6be2707e294371513b57951b
“wenodev” <logicalstark1105@gmail.com>
2020-04-20 23:27:57 +0900

    create WebConfig

--- src/main/java/com/weno/board/web/IndexController.java
+++ src/main/java/com/weno/board/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.weno.board.web;
 
+import com.weno.board.config.auth.LoginUser;
 import com.weno.board.config.auth.dto.SessionUser;
 import com.weno.board.service.posts.PostsService;
 import com.weno.board.web.dto.PostsResponseDto;
@@ -19,15 +20,15 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
+
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
-
 
         return "index";
     }
